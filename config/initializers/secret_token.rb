b93153fc9b90f2d98314c0dafcc8d7ac403d4014
Karunakar (Ruby) <revurikarna@gmail.com>
2011-07-22 17:40:16 +0530

    postgresql sample to try out Travis CI

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pg::Application.config.secret_token = '59a4acc5ae58976d35a70d0f00b97482d420017eb6cac1142eced77586cfd3d0aa434f1918aa6ea998e6beb6efd00650c3aeac7ea9bc8aaba7df1e1c62cc6ba9'
